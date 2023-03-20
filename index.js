6121140255c6eb0b580aac5a660ae5d04ed15952
TopperDEL <github_com@tparth.de>
2023-03-20 14:15:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require('http');
+const GUN = require('gun');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo, this is me!');
